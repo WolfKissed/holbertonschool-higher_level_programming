ff798efdb3af9f2ab0fee0076e568401386609d9
WolfKissed <github.com/WolfKissed>
2021-12-21 03:44:27 -0800

    gg

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    x = -1 * number
+    n = x % 10
+else:
+    n = number % 10
+if n == 0:
+    print('Last digit of {:d} is {:d} and is 0'.format(number, n))
+elif n > 5:
+    print('Last digit of {:d} is {:d} and is greater than 5'.format(number, n))
+else
+    print('Last digit of {:d} is {:d} and us kess than 6 and not 0'.format(number, n))
