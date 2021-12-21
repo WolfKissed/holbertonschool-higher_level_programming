344e755d2657bd720548258968873fcb79ba9a12
WolfKissed <github.com/WolfKissed>
2021-12-21 04:44:32 -0800

    gg

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i < j and i < 8:
-            print("{}{}, ".format(i, j), end="")
-        elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
+for j in range(10):
+    for i in range(10):
+        if j < i and j < 8:
+            print("{}{}, ".format(j, i), end="")
+        elif j == 8 and i == 9:
+            print("{}{}".format(j, i))
