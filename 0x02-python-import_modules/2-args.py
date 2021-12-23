f53a20b9c78ebc747f0d23381747103c5776a54d
WolfKissed <github.com/WolfKissed>
2021-12-23 06:13:53 -0800

    gg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
-    sum = 0
-    if length > 1:
-        for args in sys.argv:
-            if args != sys.argv[0]:
-                sum = sum + int(args)
-    print(sum)
+
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("0 arguments.")
+    elif x == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(x))
+    for i in range(x):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
