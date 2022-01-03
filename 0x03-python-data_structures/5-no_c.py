16eed92f3ab8b3ae2018f8181d7cef9f3cae09b3
WolfKissed <github.com/WolfKissed>
2022-01-03 13:32:40 -0800

    gg

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     meow = ""
     for i in my_string:
-        if not (i == 'c'):
-            meow += i
-        if not (i == 'C'):
+        if not (i == 'c' or i == 'C'):
             meow += i
     return meow
