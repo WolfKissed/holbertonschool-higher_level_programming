47fe1c5f30cbc06c1f4f87558c3d36f45e6fcc43
WolfKissed <github.com/WolfKissed>
2022-01-05 04:21:34 -0800

    gg

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,8 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-         m = a_dictionary[0]
-        for i in a_dictionary:
+         m = a_dictionary.keys[0]
+        for i in a_dictionary.items:
             if i > m:
                 m = i
         return m
