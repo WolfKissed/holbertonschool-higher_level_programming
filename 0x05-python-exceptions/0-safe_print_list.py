c961b75458150831f97d1986b4a14505acf4c17a
WolfKissed <github.com/WolfKissed>
2022-01-17 07:06:38 -0800

    gg

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    x = 0
-    for i in range(n):
+    a = 0
+    for i in range(x):
         try:
             print(my_list[i], end="")
-            x += 1
+            a += 1
         except:
             break
     print("")
-    return (x)
+    return (a)
