c29f9d7346dce512da5553ee4fe256ba2709a716
WolfKissed <github.com/WolfKissed>
2022-01-31 13:22:35 -0800

    gg

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+MyList module inherited
+"""
+
+
+class MyList(list):
+    """
+    Class
+    """
+    def print_sorted(self):
+        """
+        prints a sorted list
+        """
+        return (sorted(slef))
