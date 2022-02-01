772347ffa7bd0519d405bc89e17c53bf54ccda22
WolfKissed <github.com/WolfKissed>
2022-02-01 13:33:29 -0800

    gg

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+adds argv to a list
+"""
+import sys
+
+_save = __import__('5-save_to_json_file').save_to_json_file
+_load = __import__('6-load_from_json_file').load_from_json_file
+
+open("add_item.json", "a")
+try:
+    x = _load("add_item.json")
+except ValueError:
+    x = []
+_save(x + sys.argv[1:], "add_item.json")
