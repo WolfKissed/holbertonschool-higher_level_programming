d85e7ef89965e9ed7421723f43567c7c4aa0eb44
WolfKissed <github.com/WolfKissed>
2022-02-01 13:03:29 -0800

    gg

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+write
+"""
+
+
+def read_file(filename=""):
+    """
+    write a text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
