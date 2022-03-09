8b601def8368b25049e7603ddaa821c21be96c6b
WolfKissed <github.com/WolfKissed>
2022-03-09 03:41:34 -0800

    gg

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a server user
-CREATE USER IF NOT EXISTS user_0d_1 WITH PASSWORD = 'user_0d_1_pwd';
-GRANT * to user_0d_1;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' WITH PASSWORD = 'user_0d_1_pwd';
+GRANT * TO user_0d_1@'localhost';
