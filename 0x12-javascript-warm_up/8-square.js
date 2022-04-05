01a41e863ef692fa9fe248585c7b633a07c7b7b9
WolfKissed <github.com/WolfKissed>
2022-04-05 06:32:04 -0700

    gg

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 const x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing size');
-}
-const a = new Array(Number(size)).fill('X').join('');
-for (let i = 0; i < x; i++) {
-  console.log(a);
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
 }
