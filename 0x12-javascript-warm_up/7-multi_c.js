c5a89ec4900a11e4e312adaf200145f5219348df
WolfKissed <github.com/WolfKissed>
2022-04-05 06:14:44 -0700

    gg

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const x = 'C is fun';
 if (isNaN(process.argv[2])) {
 	console.log('Missing number of occurrences');
 }
 for (let i = 0; i < process.argv[2]; i++) {
-	console.log(x);
+	console.log('C is fun');
 }
