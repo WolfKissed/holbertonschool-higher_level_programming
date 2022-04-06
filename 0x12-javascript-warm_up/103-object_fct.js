6f523d0572e720ef9395a65ab2c1847d683bcba7
WolfKissed <github.com/WolfKissed>
2022-04-05 19:57:41 -0700

    gg

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+while (MyObject > 11) {
+  MyObject += 1;
+}
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
